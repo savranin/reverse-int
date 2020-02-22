04be9ee9c1c1af238226c2ed5c01f4018f22b87f
Dmitriy Savranin <dsavranin@gmail.com>
2020-02-22 17:23:00 +0500

    refactor: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(number) {
+    let str = number.toString();
+    let regexp = /\d/g;
+    let strm = str.match(regexp).join('');
+    let newstr = '';
+    for (let i = strm.length - 1; i > -1; i--) {
+        newstr = newstr + strm[i];
+    }
+    res = Number(newstr);
+    return res;
 }
